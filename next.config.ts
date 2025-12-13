f73e6d6571cca6831ecabdb6206516aa3d87d205
Rafael Nobre <rafael.nobre.d@gmail.com>
2025-12-13 01:49:51 +0000

    modified:   next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,12 @@ const nextConfig: NextConfig = {
         hostname: 'firebasestorage.googleapis.com',
         port: '',
         pathname: '/**',
+      }, 
+      {
+        protocol: 'https',
+        hostname: 'imgur.com',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
